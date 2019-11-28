a2fdef348dba35d739555b25ac6fa75b9d3b4130
Loris Maximina <lmaximin@ox-e4.21-school.ru>
2019-11-28 20:50:13 +0300

    I-0PA IIudop

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,32 @@
 /*   By: lmaximin <lmaximin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 20:23:49 by lmaximin          #+#    #+#             */
-/*   Updated: 2019/11/28 20:23:52 by lmaximin         ###   ########.fr       */
+/*   Updated: 2019/11/28 20:49:45 by lmaximin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FILLIT_H
+# define FILLIT_H
+# include <string.h>
+# include "../libft/libft.h"
+# include "../libft/get_next_line.h"
+
+typedef struct	s_map
+{
+	int			size;
+	char		**array;
+}				t_map;
+typedef struct	s_point
+{
+	int			x;
+	int			y;
+}				t_point;
+typedef struct	s_shape
+{
+	char		**pos;
+	int			width;
+	int			height;
+	char		value;
+}				t_shape;
+
+#endif
